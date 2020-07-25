d884cf887b72baf9bae343e120845b279da5d090
Gorka López de Torre <gorka@gorka.io>
2020-07-25 09:07:15 +0200

    Improve doc and format

--- lib/fetcher.ex
+++ lib/fetcher.ex
@@ -7,16 +7,22 @@ defmodule Fetcher do
   @doc """
   Fetch image and link tags URLs.
 
+  ## Parameters
+
+    - url: String that represents the URL to parse
+
   ## Examples
 
       iex> Fetcher.fetch("https://google.com")
       :ok
 
   """
-  @spec fetch(url()) :: :ok
+  @spec fetch(any) :: :ok | {:error, term}
   def fetch(url) when is_binary(url) do
     :ok
   end
 
-  def fetch(_url) do {:error, :invalid_url} end
+  def fetch(_url) do
+    {:error, :invalid_url}
+  end
 end
